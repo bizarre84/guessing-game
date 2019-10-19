a1b2471f760f82f7051bc229351d83f2e840d3ef
bizarre84 <bizarre.san@gmail.com>
2019-10-19 21:56:37 +0300

    feat: "Guessing game" task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        let candidate = Math.round(this.min + (this.max-this.min)/2);
+        return candidate;
     }
 
     lower() {
-
+        this.max = Math.round(this.min + (this.max-this.min)/2);
     }
 
     greater() {
-
+        this.min = Math.round(this.min + (this.max-this.min)/2);
     }
 }
 
